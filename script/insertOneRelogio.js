756662d549ff8e88c317d6d94ac1571ebf90525a
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2018-02-12 01:29:10 -0200

    pingApp: add pingApp which is responsible to check clock status

--- script/insertOneRelogio.js
+++ script/insertOneRelogio.js
@@ -5,8 +5,12 @@ const {
 
 const Relogio = getModel('Relogio');
 
-isDBReady().then(() => Relogio.create({
-  host: '192.168.15.53',
-  port: 3000,
-  interval: 30000
-}));
+isDBReady().then(async () => {
+  for (let i = 0; i < 3; i += 1) {
+    await Relogio.create({ // eslint-disable-line
+      host: 'ox1.modrp.com',
+      port: 3000,
+      interval: 30000,
+    });
+  }
+});
