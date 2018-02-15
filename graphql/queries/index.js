4e2c1ef3d95c95216065e3b82dc0f6ce5be61763
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2018-02-15 02:00:13 -0200

    graphql: implemented lazy loading to retrieve clock list

--- graphql/queries/index.js
+++ graphql/queries/index.js
@@ -1,11 +1,14 @@
 const { GraphQLObjectType } = require('graphql');
 const { clockQuery } = require('./clock.query');
+const { clocksQuery } = require('./clocks.query');
+
 
 const query = new GraphQLObjectType({
   name: 'query',
   description: 'This query return a clock',
   fields: {
     clock: clockQuery,
+    clocks: clocksQuery,
   },
 });
 
